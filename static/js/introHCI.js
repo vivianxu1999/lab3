584c75154218c9146d35cae8a3434f41b4c8f180
Yiran Xu <vivianxu1999@gmail.com>
2020-01-21 13:13:07 -0800

    added clicked and faded out effect

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,30 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+        $("#testjs").text("Please wait...");
+        $(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
+    $("a.thumbnail").click(projectClick);
 	// example: $("#div-id").click(functionToCall);
 }
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color");
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       //$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+       $(".project-description").fadeOut();
+    }
+}
+
